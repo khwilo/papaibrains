14d8ef54621ef6115a1a02886ffb2829a62bfb39
khwilo <kwhyzzy@gmail.com>
2016-11-12 16:32:16 +0300

    correct zoom level

--- js/map.js
+++ js/map.js
@@ -2,14 +2,17 @@
  * and set its geographical center and zoom
  * the center is at Isiolo, Kenya
  */
-var map = L.map('mapid').setView([1.002, 38.167], 13);
+var map = L.map('mapid').setView([1.002, 38.167], 6);
 
 /* provide the url of the map */
 mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
 /* add a map entity as a tile layer */
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-		{attribution: '&copy;' + mapLink + 'Contributors', maxZoom: 18,
+		{
+			attribution: '&copy;' + mapLink + 'Contributors', 
+			maxZoom: 18,
+			/*minZoom: 9*/
 		}).addTo(map);
 
 /* initialize the svg layer */
@@ -19,7 +22,7 @@ map._initPathRoot();
 var svg = d3.select("#map").select("svg"),
 g = svg.append("g");
 
-d3.json("papai_dev.json", function(collection) {
+d3.json("papai.json", function(collection) {
 	/* Add latitude and longitude item to each item in the dataset */
 	collection.objects.forEach(function(d) {
 		d.LatLng = new L.LatLng(d.circle.coordinates[0], 
